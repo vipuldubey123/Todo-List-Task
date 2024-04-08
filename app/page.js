4702c71a9955b7aaf690bcc36bdad6b244504c7e
Vipul Dubey <105154005+vipuldubey123@users.noreply.github.com>
2024-04-08 16:39:48 +0530

    Add files via upload

---
+++ app/page.js
@@ -0,0 +1,60 @@
+"use client"
+
+import React, { Fragment, useState } from 'react'
+import { useAuthState } from 'react-firebase-hooks/auth'
+import { auth } from '@/app/firebase/config'
+import { useRouter } from 'next/navigation';
+import Show from './components/Show'
+import Create from './components/Create'
+
+
+
+
+const Home = () => {
+
+  const [task, settask] = useState([])
+
+  const [taskname, settaskname] = useState("")
+  const [description, setdescription] = useState("")
+  const [updateTask, setupdateTask] = useState(null)
+
+
+  function updater(idx) {
+
+    setupdateTask(idx)
+
+    settaskname(task[idx].taskname)
+    setdescription(task[idx].description)
+
+
+  }
+
+  return (
+    <Fragment>
+
+      <Create
+        task={task}
+        settask={settask}
+        taskname={taskname}
+        description={description}
+        settaskname={settaskname}
+        setdescription={setdescription}
+        updateTask={updateTask}
+        setupdateTask={setupdateTask}
+      ></Create>
+
+      <Show
+        task={task}
+        settask={settask}
+        updater={updater}
+      ></Show>
+
+
+    </Fragment>
+  )
+}
+
+export default Home
+
+
+
